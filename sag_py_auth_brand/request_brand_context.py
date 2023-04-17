1d4236baf808bffec3356562b01ede0f37c65454
the-holsti <96723151+the-holsti@users.noreply.github.com>
2023-04-17 13:53:25 +0200

    KIT-2036 add prefix _request to context variables

--- sag_py_auth_brand/request_brand_context.py
+++ sag_py_auth_brand/request_brand_context.py
@@ -1,13 +1,13 @@
 from contextvars import ContextVar
 from typing import Optional
 
-brand: ContextVar[Optional[str]] = ContextVar("request_brand", default=None)
-brand_alias: ContextVar[Optional[str]] = ContextVar("request_brand_alias", default=None)
+_request_brand: ContextVar[Optional[str]] = ContextVar("request_brand", default=None)
+_request_brand_alias: ContextVar[Optional[str]] = ContextVar("request_brand_alias", default=None)
 
 
 def get_brand() -> str:
     """Gets the context local brand. This is the brand for backend logic.
-    It contains the brand alias if set and otherwise the request brand.
+    It contains the compound brand alias if set and otherwise the request brand.
     See library contextvars for details.
 
     Returns: The brand
@@ -21,7 +21,7 @@ def get_request_brand() -> str:
 
     Returns: The brand
     """
-    current_brand: Optional[str] = brand.get("")
+    current_brand: Optional[str] = _request_brand.get("")
     return current_brand or ""
 
 
@@ -31,17 +31,17 @@ def get_request_brand_alias() -> str:
 
     Returns: The brand alias
     """
-    current_brand_alias: Optional[str] = brand_alias.get("")
+    current_brand_alias: Optional[str] = _request_brand_alias.get("")
     return current_brand_alias or ""
 
 
 def set_request_brand(brand_to_set: Optional[str]) -> None:
     """Sets the context local brand. This is always the brand of the request.
     See library contextvars for details."""
-    brand.set(brand_to_set)
+    _request_brand.set(brand_to_set)
 
 
 def set_request_brand_alias(brand_alias_to_set: Optional[str]) -> None:
     """Sets the context local brand alias. This is the brand used for backend logic if it is set.
     See library contextvars for details."""
-    brand_alias.set(brand_alias_to_set)
+    _request_brand_alias.set(brand_alias_to_set)
