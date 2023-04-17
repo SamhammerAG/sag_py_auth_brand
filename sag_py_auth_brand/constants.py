62939516d545008ea513c22bf3abace62b098ca9
the-holsti <96723151+the-holsti@users.noreply.github.com>
2023-04-17 16:15:51 +0200

    KIT-2036 update debug message

--- sag_py_auth_brand/constants.py
+++ sag_py_auth_brand/constants.py
@@ -1,4 +1,4 @@
 class LogMessages:
-    MISSING_BRAND_ALIAS: str = "List of accessible brand aliases is empty."
+    MISSING_BRAND_ALIAS: str = "Request brand not in list of accessible brand aliases."
     MISSING_COMPOUND_BRAND: str = "Unambiguous role association: brand is not associated with any brand aliases."
     UNAMBIGUOUS_BRAND_ALIAS: str = "Unambiguous role association: user has access to more than one brand alias."
