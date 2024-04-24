42b37f62d34ec7f7b06334a215e9923ca8f282a7
Stefan Rom <srom@samhammer.de>
2024-04-24 10:35:56 +0200

    KIT-3375 Re add removed unit test because some of it's checks are still useful

--- tests/test_brand_jwt_auth__verify_brand.py
+++ tests/test_brand_jwt_auth__verify_brand.py
@@ -3,6 +3,7 @@ from unittest import TestCase, main
 
 import mock
 import pytest
+from fastapi import HTTPException
 from mock import Mock
 from sag_py_auth.models import Token
 
@@ -30,6 +31,25 @@ class TestVerifyBrand(TestCase):
         # Assert
         mock_set_brand_to_context.assert_called_once_with("mybrandone")
 
+    @mock.patch("sag_py_auth_brand.brand_jwt_auth.set_request_brand_to_context")
+    def test__verify_brand__where_brand_is_missing(
+        self, mock_set_brand_to_context: Mock
+    ) -> None:
+        # Arrange
+        brand_jwt_auth: BrandJwtAuth = build_sample_jwt_auth(["myEndpoint"])
+
+        resource_access: Optional[Dict[str, Any]] = {"role-brand": {"roles": ["mybrandone", "mybrandtwo"]}}
+
+        token: Token = get_token(None, resource_access)
+
+        # Act
+        with pytest.raises(HTTPException) as exception:
+            brand_jwt_auth._verify_brand(token, "mybrandthree")
+
+        # Assert
+        assert exception.value.status_code == 403
+        assert exception.value.detail == "Missing brand."
+        mock_set_brand_to_context.assert_called_once_with(None)
 
 if __name__ == "__main__":
     main()
